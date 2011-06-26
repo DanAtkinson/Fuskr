b49e832dd115cf11580cd7345457d54f3310146b
Dan Atkinson <dan.github@dan-atkinson.com>
2011-06-26 01:55:27 +0100

    * Use the latest version of jQuery from Google, and fallback to the local version if it doesn't load. * Forced images to hide, and then show once they've loaded.

--- Scripts/fuskr.js
+++ Scripts/fuskr.js
@@ -1,16 +1,16 @@
 ;var Fuskr = (function(ret){
 
-	var regex =  /^(.*?)\[(\d+)\-(\d+)\](.*)$/,
+	var regex = /^(.*?)\[(\d+)\-(\d+)\](.*)$/,
 		groupRegex = /\{\d+\}/;
-	
-	function PadString(number, stringLength, padding) {
+
+		function PadString(number, stringLength, padding) {
 		var numStr = ""+number;
 		if(!padding) return numStr;
-		
+
 		while(numStr.length < stringLength){
-			numStr = padding + numStr; 
+			numStr = padding + numStr;
 		}
-		
+
 		return numStr;
 	}
 
@@ -19,15 +19,15 @@
 			startNumber = parseInt(startNumString, 10),
 			endNumber = parseInt(endNumString, 10),
 			paddedLength = startNumString.length;
-			
-		while(startNumber <= endNumber){
+
+			while(startNumber <= endNumber){
 			var thisNumString = PadString(startNumber, paddedLength, "0"),
 				link = prefix + thisNumString + suffix;
-			
+
 			if(groupRegex.test(link)){
 				link = link.replace(new RegExp("\\\{"+groupNumber+"\\\}", 'g'), thisNumString);
 			}
-			
+
 			if(ret.IsFuskable(link)){
 				var links = ret.GetLinks(link, groupNumber+1);
 				for(var i = 0; i < links.length; i++){
@@ -38,24 +38,24 @@
 			}
 			startNumber++;
 		}
-		
+
 		return retUrls;
 	}
-	
+
 	ret.IsFuskable = function(url) {
 		return regex.test(url);
 	};
-	
+
 	ret.GetLinks = function(url, groupNumber) {
 		var links = [],
 			groupNumber = groupNumber || 0;
-			
+
 		if(ret.IsFuskable(url)){
 			var matches = regex.exec(url);
 			links = GetUrls(matches[1],matches[4], matches[2], matches[3],groupNumber);
 		}
 		return links;
 	}
-	
+
 	return ret;
 }(Fuskr || {}));
